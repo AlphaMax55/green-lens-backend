1106aab4721e4206720a13a3327bcff9b686cb8e
ÖMER YILDIRIM <oooner_@hotmail.com>
2026-03-10 01:58:02 +0300

    Change database connection settings
    Updated database connection URL to use port 6543 and added pgbouncer parameter.

--- database.py
+++ database.py
@@ -2,10 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# 🎯 SUPABASE SESSION POOLER (Sydney Hattı)
-# Şifren olan 'Eomer3255..' başarıyla mühürlendi.
-# Render'ın IPv4 üzerinden ulaşabilmesi için bu adres şarttı reis.
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres.qqkyjsafhevywgdaywvk:Eomer3255..@aws-1-ap-southeast-2.pooler.supabase.com:5432/postgres"
+# 🎯 POOLER CONNECTION (6543 Portu - IPv4 Uyumlu)
+# Not: Kullanıcı adındaki noktaya ve portun 6543 olduğuna dikkat reis!
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.qqkyjsafhevywgdaywvk:Eomer3255..@aws-1-ap-southeast-2.pooler.supabase.com:6543/postgres?pgbouncer=true"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
