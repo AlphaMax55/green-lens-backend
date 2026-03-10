e256cf7efc9225b7879f7c21f4b5c90ed2c65ba3
ÖMER YILDIRIM <oooner_@hotmail.com>
2026-03-10 03:54:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 # 🎯 iNaturalist Görüntü Tanıma Adresi
-INAT_URL = "https://api.inaturalist.org/v1/computervision/score"
+INAT_URL = "https://api.inaturalist.org/v1/identifications"
 
 @app.get("/")
 async def root():
@@ -83,5 +83,6 @@ async def predict(file: UploadFile = File(...), db: Session = Depends(get_db)):
 @app.get("/history")
 async def get_history(db: Session = Depends(get_db)):
     return db.query(TaramaGecmisi).order_by(TaramaGecmisi.tarih.desc()).limit(20).all()
+
 
 
